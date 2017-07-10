8623111dd8e626585d764e553d047039ae0aa319
JaeHo Song <thruthesky@gmail.com>
2017-07-10 23:23:21 +0800

    update for backend

--- firebase-backend.module.ts
+++ firebase-backend.module.ts
@@ -1,26 +1,26 @@
 import { NgModule } from '@angular/core';
 
-import { ApiService } from './functions/model/api/api';
-export { ApiService } from './functions/model/api/api';
+import { ApiService } from './src/model/api/api';
+export { ApiService } from './src/model/api/api';
 
 
-import { TestService } from './functions/model/test/test';
-export { TestService } from './functions/model/test/test';
+import { TestService } from './src/model/test/test';
+export { TestService } from './src/model/test/test';
 
-import { ForumService } from './functions/model/forum/forum.service';
-export { ForumService } from './functions/model/forum/forum.service';
+import { ForumService } from './src/model/forum/forum.service';
+export { ForumService } from './src/model/forum/forum.service';
 
-import { Forum } from './functions/model/forum/forum';
-export { Forum } from './functions/model/forum/forum';
-
-
-export { ERROR, isError } from './functions/model/error/error';
+import { Forum } from './src/model/forum/forum';
+export { Forum } from './src/model/forum/forum';
 
 
-export { CATEGORY, CATEGORIES, POST, POSTS, ALL_CATEGORIES } from './functions/model/forum/forum.interface';
+export { ERROR, isError } from './src/model/error/error';
 
-import { UserService } from './functions/model/user/user.service';
-export { UserService, SOCIAL_PROFILE, USER_REGISTER, USER_UPDATE } from './functions/model/user/user.service';
+
+export { CATEGORY, CATEGORIES, POST, POSTS, ALL_CATEGORIES } from './src/model/forum/forum.interface';
+
+import { UserService } from './src/model/user/user.service';
+export { UserService, SOCIAL_PROFILE, USER_REGISTER, USER_UPDATE } from './src/model/user/user.service';
 
 @NgModule({
     providers: [ ForumService, UserService, ApiService, TestService ],
